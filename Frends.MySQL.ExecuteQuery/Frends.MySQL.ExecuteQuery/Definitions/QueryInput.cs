723c1cbcae88c4f1d90a013e8e417f4ee7e9b6af
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-08-30 10:30:58 +0300

    init

---
+++ Frends.MySQL.ExecuteQuery/Frends.MySQL.ExecuteQuery/Definitions/QueryInput.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Frends.MySQL.ExecuteQuery.Definitions;
+
+/// <summary>
+/// Query input.
+/// </summary>
+public class QueryInput
+{
+    /// <summary>
+    /// Mysql connection string.
+    /// </summary>
+    /// <example>server=server;user=user;database=db;password=pw;</example>
+    [PasswordPropertyText]
+    [DisplayFormat(DataFormatString = "Text")]
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// SQL statement to execute at the data source. Usually query or name of a stored procedure.
+    /// </summary>
+    /// <example>SELECT ColumnName FROM TableName</example>
+    [DisplayFormat(DataFormatString = "Sql")]
+    public string CommandText { get; set; }
+
+    /// <summary>
+    /// Parameters for the database query.
+    /// </summary>
+    /// <example>foo, bar</example>
+    public Parameter[] Parameters { get; set; }
+}
+
+/// <summary>
+/// Set properties of parameters.
+/// </summary>
+public class Parameter
+{
+    /// <summary>
+    /// The name of the parameter
+    /// </summary>
+    /// <example>foo</example>
+    [DisplayFormat(DataFormatString = "Text")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The value of the parameter
+    /// </summary>
+    /// <example>bar</example>
+    [DisplayFormat(DataFormatString = "Text")]
+    public dynamic Value { get; set; }
+}
---
+++ Frends.MySQL.ExecuteQuery/Frends.MySQL.ExecuteQuery/Definitions/Result.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Frends.MySQL.ExecuteQuery.Definitions;
+
+/// <summary>
+/// Result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Task status.
+    /// </summary>
+    /// <example>true</example>
+    public bool Success { get; private set; }
+
+    /// <summary>
+    /// Result message.
+    /// </summary>
+    public string Message { get; private set; }
+
+    /// <summary>
+    /// Result value(s).
+    /// </summary>
+    /// <example>[{"name": "foo", "value": 123}]</example>
+    public JToken ResultJtoken { get; private set; }
+
+    internal Result(bool success, string message, JToken resultJtoken)
+    {
+        Success = success;
+        Message = message;
+        ResultJtoken = resultJtoken;
+    }
+}
