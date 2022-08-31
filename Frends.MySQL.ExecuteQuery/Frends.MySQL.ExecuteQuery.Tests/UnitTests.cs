feec8baf915c10b9e0c7931059eebd4e515e77b1
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-08-31 10:59:29 +0300

    init

--- Frends.MySQL.ExecuteQuery/Frends.MySQL.ExecuteQuery.Tests/UnitTests.cs
+++ Frends.MySQL.ExecuteQuery/Frends.MySQL.ExecuteQuery.Tests/UnitTests.cs
@@ -8,7 +8,6 @@ namespace Frends.MySQL.ExecuteQuery.Tests;
 /// Setup MySQL to docker:
 /// docker run --name some-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql
 /// </summary>
-/// 
 [TestFixture]
 public class UnitTests
 {
